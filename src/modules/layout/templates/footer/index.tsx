e1ae18910c945b9a848cc119e320108f69acb9f1
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-07-31 22:37:03 +0700

    Updated social icons

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -7,11 +7,13 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import MedusaCTA from "@modules/layout/components/medusa-cta"
 import Image from "next/image"
 import enejiLogo from "../../../../../public/images/enejistation-logo.png"
-import zaloIcon from "../../../../../public/images/zalo.svg"
 import cashIcon from "../../../../../public/images/tien-mat.png"
 import enejiCardIcon from "../../../../../public/images/the-eneji.png"
 import acbIcon from "../../../../../public/images/acb.png"
 import momoIcon from "../../../../../public/images/momo.png"
+import facebookIcon from "../../../../../public/images/facebook.png"
+import instagramIcon from "../../../../../public/images/instagram.png"
+import zaloIcon from "../../../../../public/images/zalo.png"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
 export default async function Footer() {
@@ -32,118 +34,9 @@ export default async function Footer() {
             ></Image>
           </div>
 
-          {/* <div className="w-full flex flex-wrap gap-x-6 gap-y-10 mt-4">
-            <div className="flex flex-col gap-6 flex-1 min-w-[300px] md:min-w-[calc(50%-12px)] lg:min-w-[calc(25%-18px)]">
-              <div className="font-bold">Công ty Cổ phần TMDV Quả Táo</div>
-              <div className="space-y-4">
-                <div className="">
-                  <b>Địa chỉ:</b> Số 2/29 Cao Thắng, Phường 05, Quận 3, Thành
-                  phố Hồ Chí Minh, Việt Nam.
-                </div>
-                <div className="">
-                  <b>Hotline:</b> (028) 3535 2563
-                </div>
-                <div className="">
-                  <b>Mã số doanh nghiệp:</b> 0317034495 do Sở Kế hoạch & Đầu tư
-                  TP Hồ Chí Minh cấp ngày 17/11/2021
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col gap-6 flex-1 min-w-[300px] md:min-w-[calc(50%-12px)] lg:min-w-[calc(25%-18px)]">
-              <div className="font-bold">Về Eneji Station</div>
-              <div className="flex flex-col">
-                <Link className="" href={""}>
-                  Giới thiệu về Eneji Station
-                </Link>
-                <Link className="" href={""}>
-                  Chính sách bảo mật
-                </Link>
-                <Link className="" href={""}>
-                  Chính sách trả hàng và hoàn tiền
-                </Link>
-                <Link className="" href={""}>
-                  Chính sách vận chuyển
-                </Link>
-                <Link className="" href={""}>
-                  Giải quyết khiếu nại
-                </Link>
-                <Link className="" href={""}>
-                  Hướng dẫn mua hàng
-                </Link>
-              </div>
-            </div>
-            <div className="flex flex-col gap-6 flex-1 min-w-[300px] md:min-w-[calc(50%-12px)] lg:min-w-[calc(25%-18px)]">
-              <div className="font-bold">Phương thức thanh toán</div>
-              <div className="flex flex-row gap-2">
-                  <Image
-                    src={cashIcon}
-                    alt="Tiền mặt"
-                    width={70}
-                    height={46}
-                    className="rounded-md"
-                  />
-                  <Image
-                    src={enejiCardIcon}
-                    alt="Thẻ Eneji"
-                    width={83}
-                    height={46}
-                    className="rounded-md"
-                  />
-                  <Image
-                    src={acbIcon}
-                    alt="ACB"
-                    width={92}
-                    height={46}
-                    className="rounded-md"
-                  />
-                  <Image
-                    src={momoIcon}
-                    alt="Momo"
-                    width={46}
-                    height={46}
-                    className="rounded-md"
-                  />
-              </div>
-            </div>
-            <div className="flex flex-col gap-6 flex-1 min-w-[300px] md:min-w-[calc(50%-12px)] lg:min-w-[calc(25%-18px)]">
-              <div className="font-bold">Mạng xã hội</div>
-              <div className="flex flex-row gap-3">
-                <Link
-                  href="https://www.facebook.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center"
-                >
-                  <i className="fab fa-facebook-square text-blue-600 text-4xl"></i>
-                </Link>
-                <Link
-                  href="https://www.instagram.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center"
-                >
-                  <i className="fab fa-instagram-square text-pink-500 text-4xl"></i>
-                </Link>
-                <Link
-                  href="https://zalo.me"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center"
-                >
-                  <Image
-                    src={zaloIcon}
-                    alt="Zalo"
-                    width={33}
-                    height={33}
-                    className="w-[2.063rem] h-[2.063rem] p-0 m-0"
-                  ></Image>
-                </Link>
-              </div>
-            </div>
-          </div> */}
-          {/* <div className="w-full flex flex-wrap gap-x-5 gap-y-10 mt-4">
-            <div className="min-w-[600px] bg-red-500 flex-1 p-1 flex justify-around gap-x-5 gap-y-10">
-              <div className="h-8 bg-yellow-500 flex-1">
+          <div className="w-full flex flex-wrap gap-x-5 gap-y-10 mt-4">
+            <div className="md:min-w-[400px] lg:min-w-[400px] flex-1 flex justify-around gap-x-5 gap-y-10 flex-wrap md:flex-nowrap">
+              <div className="min-w-[300px] flex-1 space-y-3 md:space-y-7">
                 <div className="font-bold">Công ty Cổ phần TMDV Quả Táo</div>
                 <div className="space-y-4">
                   <div className="">
@@ -159,7 +52,7 @@ export default async function Footer() {
                   </div>
                 </div>
               </div>
-              <div className="h-8 lg:max-w-[230px] max-w-full bg-green-500 flex-1">
+              <div className="min-w-[300px] lg:max-w-[250px] lg:min-w-[200px] max-w-none flex-1 space-y-3 md:space-y-7">
                 <div className="font-bold">Về Eneji Station</div>
                 <div className="flex flex-col">
                   <Link className="" href={""}>
@@ -183,10 +76,10 @@ export default async function Footer() {
                 </div>
               </div>
             </div>
-            <div className="min-w-[440px] lg:max-w-lg max-w-full bg-blue-500 flex-1 p-1 flex justify-around gap-x-5 gap-y-10">
-              <div className="h-8 bg-yellow-500 flex-1">
-              <div className="font-bold">Phương thức thanh toán</div>
-              <div className="flex flex-row gap-2">
+            <div className="md:min-w-[400px] lg:min-w-[440px] lg:max-w-lg max-w-full flex-1 flex justify-around gap-x-5 gap-y-10 flex-wrap md:flex-nowrap">
+              <div className="min-w-[300px] flex-1 space-y-3 md:space-y-7">
+                <div className="font-bold">Phương thức thanh toán</div>
+                <div className="flex flex-row gap-2">
                   <Image
                     src={cashIcon}
                     alt="Tiền mặt"
@@ -215,186 +108,58 @@ export default async function Footer() {
                     height={46}
                     className="rounded-md"
                   />
+                </div>
               </div>
-              </div>
-              <div className="h-8 lg:max-w-[140px] max-w-none bg-green-500 flex-1">
+              <div className="min-w-[300px] lg:min-w-min lg:max-w-[140px] max-w-none flex-1 space-y-3 md:space-y-7">
                 <div className="font-bold">Mạng xã hội</div>
-              <div className="flex flex-row gap-3">
-                <Link
-                  href="https://www.facebook.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center"
-                >
-                  <i className="fab fa-facebook-square text-blue-600 text-4xl"></i>
-                </Link>
-                <Link
-                  href="https://www.instagram.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center"
-                >
-                  <i className="fab fa-instagram-square text-pink-500 text-4xl"></i>
-                </Link>
-                <Link
-                  href="https://zalo.me"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center"
-                >
-                  <Image
-                    src={zaloIcon}
-                    alt="Zalo"
-                    width={33}
-                    height={33}
-                    className="w-[2.063rem] h-[2.063rem] p-0 m-0"
-                  ></Image>
-                </Link>
+                <div className="flex flex-row gap-3">
+                  <Link
+                    href="https://www.facebook.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center"
+                  >
+                    <Image
+                      src={facebookIcon}
+                      alt="Facebook"
+                      width={33}
+                      height={33}
+                      className="rounded-lg"
+                    ></Image>
+                  </Link>
+                  <Link
+                    href="https://www.instagram.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center"
+                  >
+                    <Image
+                      src={instagramIcon}
+                      alt="Instagram"
+                      width={33}
+                      height={33}
+                      className="rounded-lg"
+                    ></Image>
+                  </Link>
+                  <Link
+                    href="https://zalo.me"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center"
+                  >
+                    <Image
+                      src={zaloIcon}
+                      alt="Zalo"
+                      width={33}
+                      height={33}
+                      className="rounded-lg"
+                    ></Image>
+                  </Link>
+                </div>
               </div>
-              </div>
-            </div>
-          </div> */}
-          <div className="w-full flex flex-wrap gap-x-5 gap-y-10 mt-4">
-            <div className="min-w-[600px] bg-red-500 flex-1 p-1 flex justify-around gap-x-5 gap-y-10">
-              <div className="h-8 bg-yellow-500 flex-1"></div>
-              <div className="h-8 lg:max-w-[230px] max-w-full bg-green-500 flex-1"></div>
-            </div>
-            <div className="min-w-[440px] lg:max-w-lg max-w-full bg-blue-500 flex-1 p-1 flex justify-around gap-x-5 gap-y-10">
-              <div className="h-8 bg-yellow-500 flex-1"></div>
-              <div className="h-8 lg:max-w-[140px] max-w-none bg-green-500 flex-1"></div>
             </div>
           </div>
 
-          {/* <div>
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
-            >
-              Medusa Store
-            </LocalizedClientLink>
-          </div>
-
-          <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
-            {product_categories && product_categories?.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base">
-                  Categories
-                </span>
-                <ul
-                  className="grid grid-cols-1 gap-2"
-                  data-testid="footer-categories"
-                >
-                  {product_categories?.slice(0, 6).map((c) => {
-                    if (c.parent_category) {
-                      return
-                    }
-
-                    const children =
-                      c.category_children?.map((child) => ({
-                        name: child.name,
-                        handle: child.handle,
-                        id: child.id,
-                      })) || null
-
-                    return (
-                      <li
-                        className="flex flex-col gap-2 text-ui-fg-subtle txt-small"
-                        key={c.id}
-                      >
-                        <LocalizedClientLink
-                          className={clx(
-                            "hover:text-ui-fg-base",
-                            children && "txt-small-plus"
-                          )}
-                          href={`/categories/${c.handle}`}
-                          data-testid="category-link"
-                        >
-                          {c.name}
-                        </LocalizedClientLink>
-                        {children && (
-                          <ul className="grid grid-cols-1 ml-3 gap-2">
-                            {children &&
-                              children.map((child) => (
-                                <li key={child.id}>
-                                  <LocalizedClientLink
-                                    className="hover:text-ui-fg-base"
-                                    href={`/categories/${child.handle}`}
-                                    data-testid="category-link"
-                                  >
-                                    {child.name}
-                                  </LocalizedClientLink>
-                                </li>
-                              ))}
-                          </ul>
-                        )}
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )}
-            {collections && collections.length > 0 && (
-              <div className="flex flex-col gap-y-2">
-                <span className="txt-small-plus txt-ui-fg-base">
-                  Collections
-                </span>
-                <ul
-                  className={clx(
-                    "grid grid-cols-1 gap-2 text-ui-fg-subtle txt-small",
-                    {
-                      "grid-cols-2": (collections?.length || 0) > 3,
-                    }
-                  )}
-                >
-                  {collections?.slice(0, 6).map((c) => (
-                    <li key={c.id}>
-                      <LocalizedClientLink
-                        className="hover:text-ui-fg-base"
-                        href={`/collections/${c.handle}`}
-                      >
-                        {c.title}
-                      </LocalizedClientLink>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base">Medusa</span>
-              <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
-                <li>
-                  <a
-                    href="https://github.com/medusajs"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    GitHub
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://docs.medusajs.com"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Documentation
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/medusajs/nextjs-starter-medusa"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover:text-ui-fg-base"
-                  >
-                    Source code
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div> */}
         </div>
         <div className="flex w-full mb-16 justify-start text-ui-fg-muted">
           <Text className="txt-compact-small">
